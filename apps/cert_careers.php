ea7257d7fd298fa7ab19d5c30ec8e73db9c4c453
pacpix <julia_stachofsky@protonmail.com>
2022-10-27 20:18:31 +0000

    locking down access/apache config

---
+++ apps/cert_careers.php
@@ -0,0 +1,21 @@
+<?php
+
+include('../db_credentials.php');
+
+echo "<h2>" . $user . "</h2>";
+echo "<h2> test </h2>";
+
+/*
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  echo "<h2>TODO</h2><ol>"; 
+  foreach($db->query("SELECT content FROM $table") as $row) {
+    echo "<li>" . $row['content'] . "</li>";
+  }
+  echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+*/
+?>
